2e45f6a1c14e4592a10e8179c229580847861b7d
vfedotova418-png <vfedotova418@gmail.com>
2026-05-16 09:16:33 +0300

    Diff color themes added

---
+++ lib/models/scene_theme.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+enum SceneTheme {
+  oceanSpace,
+  sunsetDeep,
+  auroraForest,
+}
+
+extension SceneThemeExtension on SceneTheme {
+  String get label {
+    switch (this) {
+      case SceneTheme.oceanSpace:
+        return 'Ocean Space';
+      case SceneTheme.sunsetDeep:
+        return 'Sunset Deep';
+      case SceneTheme.auroraForest:
+        return 'Aurora Forest';
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case SceneTheme.oceanSpace:
+        return Icons.water;
+      case SceneTheme.sunsetDeep:
+        return Icons.wb_twighlight;
+      case SceneTheme.auroraForest:
+        return Icons.forest;
+    }
+  }
+
+  List<Color> get backgroundColors {
+    switch (this) {
+      case SceneTheme.oceanSpace:
+        return const [
+          Color(0xFF0A0E27),
+          Color(0xFF111B3D),
+          Color(0xFF0F3460),
+          Color(0xFF0A1628),
+        ];
+      case SceneTheme.sunsetDeep:
+        return const [
+          Color(0xFF1A0A2E),
+          Color(0xFF3D1538),
+          Color(0xFF6B2C3E),
+          Color(0xFF1C0F1A),
+        ];
+      case SceneTheme.auroraForest:
+        return const [
+          Color(0xFF0A1A0F),
+          Color(0xFF0F2B1D),
+          Color(0xFF1A4A3A),
+          Color(0xFF0C1F14),
+        ];
+    }
+  }
+
+  //add diff colors for glow and jellyfish later
+}
